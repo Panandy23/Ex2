2d1db3f6afe382f8e75a6bedda7d3e2d18450691
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-10-14 10:46:52 +0200

    obliczanie pola kuli i zaokrąglanie zrobione

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        double r;
+        double v;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj długość promienia: ");
+
+        r = scanner.nextDouble();
+
+        v = 4/3 * Math.PI * Math.sqrt(r)*r;
+
+        System.out.println("Pole kuli wynosi: ");
+        System.out.printf("%.2f%n", v);
     }
 }
